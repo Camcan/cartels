5de4f4d2175a683199ca93af407a85840fa6ad15
Developer <mr.campbell.hawkes@gmail.com>
2017-12-23 00:55:42 +1300

    Impliment Bulma framework & initial admin menu

--- frontend/src/components/admin/index.js
+++ frontend/src/components/admin/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-var conf = require('../../conf.js');
+import { baseUrl } from '../../config/api.js';
+import Menu from './menu.js';
+import Content from './content.js';
 
 export default class Admin extends Component {
    constructor(props){ 
       super(props);
       this.state = {
-         ...props
+         ...props,
+         selectedPage: "Explore"
       }
    }
    componentWillMount(){
@@ -14,9 +17,20 @@ export default class Admin extends Component {
    componentWillReceiveProps(newProps){
    
    }
+   selectPage(page){
+      this.setState({selectedPage: page})
+   }
    render(){
       return (
-         <h2> HELLO WORLD </h2>
-      );
+         <div style={{width: "100%"}}>
+            <h2>Admin Panel</h2>
+            <div style={{height: "100%", display: "flex", flexFlow:"row nowrap"}}>
+               <Menu selectPage={this.selectPage.bind(this)} selected={this.state.selectedPage}/>
+               <Content>
+
+               </Content>
+            </div>
+         </div>
+         );
    }
 }
