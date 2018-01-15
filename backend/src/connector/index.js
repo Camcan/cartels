025f576a8fe2a30b2b77b49ad3f91c45f0381632
Developer <mr.campbell.hawkes@gmail.com>
2018-01-15 19:15:12 +1300

    API routes: Can create company & get companies collection

--- backend/src/connector/index.js
+++ backend/src/connector/index.js
@@ -9,20 +9,32 @@ class Connector extends EventEmitter{
    constructor(config){
       super();
       mongodb.connect(config.mongo, (err, db) => {
-         (err, cons) => {
             if(!err){
-               this._pool = cons[0];
-               this._db = cons[1];
+               this._db = db;
                this.emit('ready');
             }else{
                this.emit('error', err);
             }
-         }
       });           
    }
 
+   getCompanies(cb){
+      this._db.collection('companies').find({}).toArray((err, result) => {
+         cb(err, result);
+      });
+   }
 
+   createCompany(co, cb){	
+      var company = {
+         id: uuid.v4(), 
+         name: co.name,
+         est: co.est
+      };
 
+      this._db.collection('companies').insertOne(company, (err, res) => {
+         cb(err);
+      });   
+   }
    // Auth User
    authAdmin(cb){
    }
--- backend/src/server.js
+++ backend/src/server.js
@@ -14,8 +14,25 @@ c.on('ready', () => {
 }); 
 
 // Where me routes!?
-
-app.listen(3000, function () {
+app.get('/api/companies', (req, res)=>{
+   c.getCompanies((err, companies)=>{
+      if (err) res.status(300).send(err) 
+      else res.status(200).send(companies)
+   })
+})
+app.post('/api/companies/create', (req, res)=>{
+   console.log("BODY:", req.body);
+   let company = {
+      name: req.body.name,
+      est: req.body.est
+   }
+   console.log("COMPANY:", company);
+   c.createCompany(company, (err,result)=>{
+      if (err) res.status(300).send(err) 
+      else res.status(200).send(result)
+   })
+})
+app.listen(3000, (req, res)=>{
      console.log('Listening for magick on port 3000...')
 })
 
